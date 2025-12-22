bcb76a44b37a55180bd71fbae9e9e9275dad9ddd
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2025-12-22 16:49:15 +0530

    added delete function

--- api/axiosInstance.js
+++ api/axiosInstance.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.0.101:3001/api", // android emulator
+  baseURL: "https://finservice.perman.hmmbo.com/api", // android emulator
 });
 
 // Finance Service (3001)
 export const financeApi = axios.create({
-  baseURL: "http://192.168.0.101:3001/api",
+  baseURL: "https://finservice.perman.hmmbo.com/api",
 });
 
 // AI Service (3002)
 export const aiApi = axios.create({
-  baseURL: "http://192.168.0.101:3002/",
+  baseURL: "https://aiservice.perman.hmmbo.com/",
 });
